c7690b190c37e207c28d6b16893f40e683c19dc0
Dipen Sagar <dipenrique@gmail.com>
2025-10-23 00:39:58 +0530

    Update google map loc to Dubai

--- blocks/store-locator/locationMap.js
+++ blocks/store-locator/locationMap.js
@@ -9,7 +9,7 @@ class LocationMap extends Component {
   componentDidMount() {
     this.loadGoogleMapsApi().then(() => {
       this.map = new google.maps.Map(this.mapContainer, {
-        center: { lat: -34.397, lng: 150.644 },
+        center: { lat: 25.2048, lng: 55.2708 },
         zoom: 8,
         disableDefaultUI: true,
         keyboardShortcuts: false,
